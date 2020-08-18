3d15678fa58bfda962acd06e353387c3a0172ce5
Ilya <ilyakurilev@gmail.com>
2020-08-18 13:44:00 +0300

    Home work of the 1st lesson added

---
+++ lessons/1/HomeWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Hello, {0}!", name);
+            Thread.Sleep(5000);
+            Console.WriteLine("Bye, {0}!", name);
+            Console.ReadKey();
+        }
+    }
+}
