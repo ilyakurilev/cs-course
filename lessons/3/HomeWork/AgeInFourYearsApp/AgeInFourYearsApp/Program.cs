e6d179e5d8757c3f43d7ffbd8b2565a089eac53a
Ilya <ilyakurilev@gmail.com>
2020-08-25 17:37:40 +0300

    Home work of the 3rd lesson added to the testing branch

---
+++ lessons/3/HomeWork/AgeInFourYearsApp/AgeInFourYearsApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AgeInFourYearsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPerson = 3;
+
+            string[] names = new string[numberOfPerson];
+            int[] ages = new int[numberOfPerson];
+
+            for (int i = 0; i < numberOfPerson; i++)
+            {
+                Console.Write("Enter the name: ");
+                names[i] = Console.ReadLine();
+                Console.Write($"Enter {names[i]}'s age: ");
+                ages[i] = System.Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n*************************************\n");
+
+            for (int i = 0; i < numberOfPerson; i++)
+            {
+                Console.WriteLine($"Name: {names[i]}, age in 4 years: {ages[i] + 4}");
+            }
+
+            Console.WriteLine("\nPress any key...");
+            Console.ReadKey(true);
+        }
+    }
+}
