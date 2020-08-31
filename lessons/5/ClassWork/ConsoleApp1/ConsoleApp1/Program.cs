d5c93c5d5c7b8f1dc609ec536c67e35ccc69ff23
Anton3topora <k.grosheva.2000@gmail.com>
2020-08-31 21:17:33 +0300

    class work

---
+++ lessons/5/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write("Enter the age from 1 to 100: ");
+            int age = System.Int32.Parse(Console.ReadLine());
+           
+            
+            if (age > 200)
+            {
+                throw new Exception("Number is greather than 200!");         
+            }
+
+
+
+            if (age < 1 || age > 100)
+            {
+                Console.WriteLine("Error input!");
+            }
+            if (age % 10 == 1 && age / 10 != 1)
+            {
+                Console.WriteLine("{0} год", age);
+            }
+            else if ((age % 10 <= 4) && age / 10 != 1)
+            {
+                Console.WriteLine("{0} года", age);
+            }
+            else
+            {
+                Console.WriteLine("{0} лет", age);
+            }*/
+            double firstNumber;
+            double secondNumber;
+            char operation;
+
+            while (true) {
+                try
+                {
+                    Console.Write("Enter the first number: ");
+                    firstNumber = System.Double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+                    Console.Write("Enter the second number: ");
+                    secondNumber = System.Double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+                    Console.Write("Enter the operation (+, -, * /): ");
+                    operation = System.Char.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Incorrect input, try again");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Incorrect input, try again");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Incorrect input, try again");
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect input, try again");
+                }
+            }
+
+            double? result = operation switch
+            {
+                '+' => firstNumber + secondNumber,
+                '-' => firstNumber - secondNumber,
+                '*' => firstNumber * secondNumber,
+                '/' => firstNumber / secondNumber,
+                _ => null
+            };
+
+            if (result != null)
+            {
+                Console.WriteLine($"{firstNumber} {operation} {secondNumber} = {result}");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect operation : {0}", operation);
+            }
+
+        }
+    }
+}
