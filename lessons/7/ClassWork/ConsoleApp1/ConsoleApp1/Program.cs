ef69e5f0373a464b3d592eaeb48495cfda4fe0ef
Anton3topora <k.grosheva.2000@gmail.com>
2020-09-07 21:33:04 +0300

    class work lesson 7

---
+++ lessons/7/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "     lorem     ipsum    dolor    sit   amet  ";
+            /*var array = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            array[1] = array[1].ToUpper();
+            var result = string.Join(' ', array);
+            Console.WriteLine(result);
+
+            string temp = text.TrimEnd();
+            string result2 = temp.Substring(0, temp.LastIndexOf(' ') + 1).TrimEnd();
+            Console.WriteLine(result2);*/
+            var sb = new StringBuilder(text);
+        }
+    }
+}
