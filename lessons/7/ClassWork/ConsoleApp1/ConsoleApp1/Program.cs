6fde244c517cf6c8b7914964a74514c8b2838c24
Ilya <ilyakurilev@gmail.com>
2020-09-08 18:07:08 +0300

    home work for lesson 7 is added

--- lessons/7/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
+++ lessons/7/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace ConsoleApp1
@@ -16,7 +17,101 @@ namespace ConsoleApp1
             string temp = text.TrimEnd();
             string result2 = temp.Substring(0, temp.LastIndexOf(' ') + 1).TrimEnd();
             Console.WriteLine(result2);*/
+
+
+            /*var sb = new StringBuilder(text);
+            var count = 0;
+            for (var i = 0; i < sb.Length;)
+            {
+                if (char.IsWhiteSpace(sb[i]))
+                {
+                    sb.Remove(i, 1);
+                }
+                else
+                {
+                    while (char.IsLetter(sb[i]))
+                    {
+                        if (count == 1)
+                        {
+                            sb[i] = char.ToUpper(sb[i]);
+                        }
+                        i++;
+                    }
+                    count++;
+                }
+            }
+            Console.WriteLine(sb);*/
             var sb = new StringBuilder(text);
+            Trim(sb);
+            RemoveExtraSpaces(sb);
+            SecondWordToUpper(sb);
+            Console.WriteLine(sb);
         }
+
+        static void Trim(StringBuilder sb)
+        {
+            while (char.IsWhiteSpace(sb[0]))
+            {
+                sb.Remove(0, 1);
+            }
+
+            while (char.IsWhiteSpace(sb[sb.Length - 1]))
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+        }
+
+        static void RemoveExtraSpaces(StringBuilder sb)
+        {
+            for (var i = 0; i < sb.Length; i++)
+            {
+                if (char.IsWhiteSpace(sb[i]))
+                {
+                    while (char.IsWhiteSpace(sb[i]))
+                    {
+                        sb.Remove(i, 1);
+                    }
+                    sb.Insert(i, ' ');
+                }
+            }
+        }
+
+        static void SecondWordToUpper(StringBuilder sb)
+        {
+            var countWords = 0;
+            for (var i = 0; i < sb.Length;)
+            {
+                if (countWords > 1)
+                {
+                    break;
+                }
+                
+                while (char.IsLetter(sb[i]))
+                {
+                    if (countWords == 1)
+                    {
+                        sb[i] = char.ToUpper(sb[i]);
+                    }
+                    i++;
+                }
+                i++;
+                countWords++;
+            }
+            var i = 0;
+            while (countWords <= 1 && i < sb.Length)
+            {
+                while (char.IsLetter(sb[i]))
+                {
+                    if (countWords == 1)
+                    {
+                        sb[i] = char.ToUpper(sb[i]);
+                    }
+                    i++;
+                }
+                i++;
+                countWords++;
+            }
+        }
+
     }
 }
---
+++ lessons/7/HomeWork/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите непустую строку:");
+            string line;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("> ");
+                    line = Console.ReadLine();
+                    if (line.Trim() == "")
+                    {
+                        Console.WriteLine("Вы ввели пустую строку, попробуйте ещё раз: ");
+                        continue;
+                    }
+                    break;
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Вы ввели null строку, попробуйте ещё раз:");
+                }
+            }
+
+            var sb = new StringBuilder();
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                sb.Append(char.ToLower(line[i]));
+            }
+
+            Console.Write("> ");
+            Console.WriteLine(sb);
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey(true);
+        }
+    }
+}
