156cdeb5a4c5d55bf2c5b4be96c295a712132724
Ilya <ilyakurilev@gmail.com>
2020-09-08 18:15:34 +0300

    home work for lesson 7 is added

---
+++ lessons/7/HomeWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку из нескольких слов: ");
+            string[] words;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("> ");
+                    words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    if (words.Length < 2)
+                    {
+                        Console.WriteLine("Слишком мало слов, попробуйте ещё раз:");
+                        continue;
+                    }
+                    break;
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Вы ввели null строку, попробуйте ещё раз:");
+                }
+            }
+
+
+            var numberWordsA = 0;
+            foreach (var word in words)
+            {
+                if (char.ToLower(word[0]) == 'a' || char.ToLower(word[0]) == 'а')
+                {
+                    numberWordsA++;
+                }
+            }
+
+            Console.WriteLine($"Количество слов, начинающихся с буквы \'A\': {numberWordsA}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey(true);
+        }
+    }
+}
