7cc7dbb9d9917e2e7db6d198c8ad0e6dad6dc320
Ilya <ilyakurilev@gmail.com>
2020-09-23 13:46:30 +0300

    home work for lesson 11

---
+++ lessons/10/HomeWork/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new Person[3];
+
+            for (var i = 0; i < persons.Length; i++)
+            {
+                try
+                {
+                    Console.Write($"Enter name {i}: ");
+                    var name = Console.ReadLine();
+                    Console.Write($"Enter age {i}: ");
+                    var age = int.Parse(Console.ReadLine());
+                    persons[i] = new Person()
+                    {
+                        Name = name,
+                        Age = age
+                    };
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    i--;
+                    Console.WriteLine("Try again!");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    i--;
+                    Console.WriteLine("Try again!");
+                }
+            }
+
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.Information);
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ lessons/11/HomeWork/ConsoleApp/ConsoleApp1/ReminderItem.Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    partial class ReminderItem
+    {
+        public void WriteProperties()
+        {
+            Console.WriteLine("____");
+            Console.WriteLine($"AlarmDate: {AlarmDate}");
+            Console.WriteLine($"AlarmMessage: {AlarmMessage}");
+            Console.WriteLine($"TimeToAlarm: {TimeToAlarm}");
+            Console.WriteLine($"IsOutdated: {IsOutdated}");
+            Console.WriteLine("____");
+        }
+    }
+}
