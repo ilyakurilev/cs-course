d24d2597b524b9476ac4873ebb0952197c1bf91e
Ilya <ilyakurilev@gmail.com>
2020-09-23 15:43:36 +0300

    home work for lesson 10

---
+++ lessons/10/HomeWork/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new Person[3];
+
+            var count = 0;
+            while (count < persons.Length)
+            {
+                try
+                {
+                    var person = new Person();
+                    Console.Write($"Enter name {count}: ");
+                    person.Name = Console.ReadLine();
+                    Console.Write($"Enter age {count}: ");
+                    person.Age = int.Parse(Console.ReadLine());
+                    persons[count++] = person;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Try again!");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Try again!");
+                }
+            }
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.Information);
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
