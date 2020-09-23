709a32c3dae42abacf070f9519ab971f37f46db1
Ilya <ilyakurilev@gmail.com>
2020-09-23 14:15:35 +0300

    home work for lesson 11

---
+++ lessons/11/HomeWork/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reminderItem1 = new ReminderItem(DateTimeOffset.Parse("13:45"), "first reminder");
+            var reminderItem2 = new ReminderItem(DateTimeOffset.Parse("12:45"), "second reminder");
+
+            reminderItem1.WriteProperties();
+            reminderItem2.WriteProperties();
+        }
+    }
+}
