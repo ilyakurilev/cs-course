4e49349bfa5ed379a9dd868f21e74c36ef2dd4fb
Ilya <ilyakurilev@gmail.com>
2020-09-29 17:48:22 +0300

    home work for lesson 13

---
+++ lessons/13/HomeWork/ConsoleApp/ConsoleApp1/FileLogWriter.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace ConsoleApp1
+{
+    class FileLogWriter : AbstractLogWriter
+    {
+        private readonly string _fileName;
+
+        public FileLogWriter(string path = "log.txt")
+        {
+            _fileName = path;
+            File.WriteAllText(_fileName, "");
+        }
+
+        protected override void Write(string message, LogType logType)
+        {
+            var formattedMessage = FormatMessage(message, logType) + "\n";
+            File.AppendAllText(_fileName, formattedMessage);
+        }
+    }
+}
---
+++ lessons/13/HomeWork/ConsoleApp/ConsoleApp1/MultipleLogWriter.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    class MultipleLogWriter : AbstractLogWriter
+    {
+        public ILogWriter[] LogWriters { get; private set; }
+
+
+        public MultipleLogWriter(params ILogWriter[] logWriters)
+        {
+            LogWriters = logWriters;
+        }
+
+
+        protected override void Write(string message, LogType logType)
+        {
+            switch (logType)
+            {
+                case LogType.Info:
+                    foreach (var logWriter in LogWriters)
+                    {
+                        logWriter.LogInfo(message);
+                    }
+                    break;
+                case LogType.Warning:
+                    foreach (var logWriter in LogWriters)
+                    {
+                        logWriter.LogWarning(message);
+                    }
+                    break;
+                case LogType.Error:
+                    foreach (var logWriter in LogWriters)
+                    {
+                        logWriter.LogError(message);
+                    }
+                    break;
+            }
+        }
+    }
+}
