d6d183e255f067e1fe7a7c58fcfa89244e851a73
ilyakurilev <ilyakurilev@gmail.com>
2020-09-30 21:45:31 +0300

    class work for lesson 14

---
+++ lessons/14/ClassWork/ConsoleApp/ConsoleApp1/ErrorList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class ErrorList : IDisposable, IEnumerable<string>
+    {
+        private List<string> _errors;
+
+        public string Category { get; }
+        
+        public ErrorList(string category)
+        {
+            Category = category;
+            _errors = new List<string>();
+        }
+
+        public void Add(string errorMessage)
+        {
+            _errors.Add(errorMessage);
+        }
+
+        public void Dispose()
+        {
+            if (_errors == null)
+            {
+                return;
+            }
+
+            _errors.Clear();
+            _errors = null;
+        }
+
+        public IEnumerator<string> GetEnumerator() =>
+            _errors.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            _errors.GetEnumerator();
+    }
+}
---
+++ lessons/14/ClassWork/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var errors = new ErrorList("Warning"))
+            {
+                errors.Add("fist warning");
+                errors.Add("second warning");
+                errors.Add("third warning");
+
+                foreach (var error in errors)
+                {
+                    Console.WriteLine($"{errors.Category}: {error}");
+                }
+            }
+        }
+    }
+}
