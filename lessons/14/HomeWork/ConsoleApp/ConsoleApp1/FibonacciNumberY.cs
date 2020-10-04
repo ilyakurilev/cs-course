57c7d210d3db591dd25500e44a9e5f6e556653b8
Ilya <ilyakurilev@gmail.com>
2020-10-04 13:19:59 +0300

    home work for lesson 14

---
+++ lessons/14/HomeWork/ConsoleApp/ConsoleApp1/FibonacciNumberY.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class FibonacciNumberY : IEnumerable<int>
+    {
+        private readonly int _number;
+
+        public FibonacciNumberY(int number)
+        {
+            _number = number;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            var _current = 1;
+            var _prev = 0;
+            for (int i = 0; i < _number; i++)
+            {
+
+                var temp = _current;
+                _current = _prev;
+                _prev = temp + _current;
+                yield return _current;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ lessons/14/HomeWork/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(new FibonacciNumber(10));
+            Print(new FibonacciNumberY(15));
+        }
+
+        static void Print(IEnumerable<int> enumerable) 
+        {
+            Console.Write("[");
+            foreach (var e in enumerable)
+            {
+                Console.Write($" {e} ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
