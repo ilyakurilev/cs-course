7bf882db70d03c08badc5174f74018f98b1721e0
ilyakurilev <ilyakurilev@gmail.com>
2020-10-05 22:10:16 +0300

    class work for lesson 15

---
+++ lessons/15/ClassWork/ConsoleApp/ConsoleApp1/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public delegate double CircleDelegate(double a);
+
+    public sealed class Circle
+    {
+        private readonly double _radius;
+
+        public Circle(double radius) =>
+            _radius = radius;
+        
+
+        public double Calculate(Func<double, double> operation) =>
+             operation(_radius);
+
+    }
+}
---
+++ lessons/15/ClassWork/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle(5);
+
+            Console.WriteLine($"Length: {circle.Calculate(CircleLength)}");
+            Console.WriteLine($"Square: {circle.Calculate(CircleArea)}");
+        }
+
+        static double CircleLength(double radius) =>
+             2 * Math.PI * radius;
+
+
+        static double CircleArea(double radius) =>
+            Math.PI * Math.Pow(radius, 2);
+        
+    }
+}
