f8d3e3c9f6e119beeecc2b0e6edfe93290cd1bca
ilyakurilev <ilyakurilev@gmail.com>
2020-10-12 21:44:11 +0300

    class work for lesson 17

---
+++ lessons/17/ClassWork/ConsoleApp/ConsoleApp1/Generator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class DataGeneratedEventArgs : EventArgs
+    {
+        public byte[] Data { get; set; }
+    }
+
+    class Generator
+    {
+        public event EventHandler<DataGeneratedEventArgs> DataGenerated;
+
+        public void Generate(int count)
+        {
+            var data = new byte[count];
+            var random = new Random();
+            random.NextBytes(data);
+            DataGenerated?.Invoke(this, new DataGeneratedEventArgs { Data = data });
+        }
+    }
+}
---
+++ lessons/17/ClassWork/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new Generator();
+            generator.DataGenerated += (sender, data) => File.WriteAllBytes("file", data.Data);
+
+            generator.Generate(100);
+        }
+    }
+}
