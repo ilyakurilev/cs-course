92a39b19023dc25fac99d11692633b886e05fbe0
ilya <ilyakurilev@gmail.com>
2020-11-15 20:12:30 +0300

    implemented Reminder.Storage.WebApi

---
+++ projects/live/Reminder/Reminder.Storage.WebApi/Dto/ReminderItemDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Reminder.Storage.WebApi.Dto
+{
+    class ReminderItemDto
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public ReminderItemStatus Status { get; set; }
+        public string Message { get; set; }
+        public string ContactId { get; set; }
+    }
+}
---
+++ projects/live/Reminder/Reminder.Storage.WebApi/ReminderStorage.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace Reminder.Storage.WebApi
+{
+    using Reminder.Storage.Exceptions;
+    using Reminder.Storage.WebApi.Dto;
+    using System.Linq;
+
+    class ReminderStorage : IReminderStorage
+    {
+        private const string ApiPrefix = "/api/reminders";
+
+        private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        private readonly HttpClient _client;
+
+        public ReminderStorage(string endpoint)
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(endpoint)
+            };
+        }
+
+        public ReminderStorage(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public void Add(ReminderItem item)
+        {
+            var json = JsonSerializer.Serialize(item, SerializerOptions);
+            var content = new StringContent(json);
+            
+            var response = _client.PostAsync(ApiPrefix, content)
+                .GetAwaiter()
+                .GetResult();
+            
+            if (response.StatusCode == HttpStatusCode.Conflict)
+            {
+                throw new ReminderItemAlreadyExistsException(item.Id);
+            }
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        public ReminderItem[] Find(DateTimeOffset dateTime, ReminderItemStatus status = ReminderItemStatus.Created)
+        {
+            var response = _client.GetAsync($"{ApiPrefix}?dateTime={dateTime}&status={status}")
+                .GetAwaiter()
+                .GetResult();
+
+            response.EnsureSuccessStatusCode();
+
+            var json = response.Content.ReadAsStringAsync()
+                .GetAwaiter()
+                .GetResult();
+
+            var dtoItems = JsonSerializer.Deserialize<ReminderItemDto[]>(json, SerializerOptions);
+
+            return dtoItems
+                .Select(dto => new ReminderItem(
+                    dto.Id,
+                    dto.Status,
+                    dto.DateTime,
+                    dto.Message,
+                    dto.ContactId)
+                )
+                .ToArray();
+        }
+
+        public ReminderItem Get(Guid id)
+        {
+            var response = _client.GetAsync($"{ApiPrefix}/{id:N}")
+                .GetAwaiter()
+                .GetResult();
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                throw new ReminderItemNotFoundException(id);
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            var json = response.Content.ReadAsStringAsync()
+                .GetAwaiter()
+                .GetResult();
+            var dto = JsonSerializer.Deserialize<ReminderItemDto>(json, SerializerOptions);
+
+            return new ReminderItem(
+                dto.Id,
+                dto.Status,
+                dto.DateTime,
+                dto.Message,
+                dto.ContactId);
+        }
+
+        public void Update(ReminderItem item)
+        {
+            var json = JsonSerializer.Serialize(item);
+            var content = new StringContent(json);
+
+            var response = _client.PutAsync($"{ApiPrefix}/{item.Id:N}", content)
+                .GetAwaiter()
+                .GetResult();
+
+            if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new ReminderItemNotFoundException(item.Id);
+            }
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
