1796300463fc25870f99af4b8a39546115daec1a
ilya <ilyakurilev@gmail.com>
2020-11-22 23:17:44 +0300

    some changes

--- projects/live/Reminder/Reminder.WebApi/Startup.cs
+++ projects/live/Reminder/Reminder.WebApi/Startup.cs
@@ -5,15 +5,19 @@ using Microsoft.Extensions.Hosting;
 
 namespace Reminder.WebApi
 {
+    using Microsoft.AspNetCore.Http;
     using Reminder.Storage;
+    using Reminder.Storage.Exceptions;
     using Reminder.Storage.Memory;
+    using System;
+    using System.Threading.Tasks;
 
     public class Startup
     { 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<IReminderStorage, ReminderStorage>();
+            services.AddSingleton<IAsyncReminderStorage, AsyncReminderStorage>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -23,9 +27,26 @@ namespace Reminder.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(ReminderExceptionHandling);
             app.UseRouting();
-
             app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
         }
+
+        private static async Task ReminderExceptionHandling(HttpContext context, Func<Task> next)
+        {
+            try
+            {
+                await next();
+            }
+            catch (ReminderItemNotFoundException)
+            {
+                context.Response.StatusCode = 404;
+            }
+            catch (ReminderItemAlreadyExistsException)
+            {
+                context.Response.StatusCode = 409;
+            }
+        }
+
     }
 }
